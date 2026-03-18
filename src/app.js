835affdee3cd8a3a10899de59940ba90a81818a7
krsnaa04 <00rajkrishna99@gmail.com>
2026-03-18 16:22:35 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express"
+import morgan from "morgan"
+import authRouter from "./routes/auth.route.js"
+import cookieParser from "cookie-parser"
+
+
+const app = express()
+
+app.use(cookieParser())
+app.use(express.json())
+app.use(morgan("dev"))
+
+app.use("/api/auth",authRouter)
+
+export default app
